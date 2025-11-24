e3703964302dbdebd9564f588eae2e7965cbf267
GeraldMarcell <93854071+GeraldMarcell@users.noreply.github.com>
2025-11-24 19:01:33 +0700

    update 1 geral

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'screens/onboarding_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
+import 'screens/start_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,11 +13,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+      ),
       initialRoute: '/onboarding',
       routes: {
         '/onboarding': (context) => OnboardingScreen(),
         '/login': (context) => LoginScreen(),
         '/home': (context) => HomeScreen(),
+        '/startpage': (context) =>  StartPage()
       },
     );
   }
