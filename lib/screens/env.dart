e6555135a805f94ef49017dc6e784235a78e050b
AryaWiragunaDwiputra <dwiputraarya31@gmail.com>
2025-12-03 18:40:34 +0700

    leaderboardd

--- lib/screens/env.dart
+++
@@ -1,5 +0,0 @@
-class Env {
-  static const String supabaseUrl = 'https://onupklziilyzuunffeaw.supabase.co';
-  static const String supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9udXBrbHppaWx5enV1bmZmZWF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5Njg3NDQsImV4cCI6MjA3OTU0NDc0NH0.gy_Kit5rQJV45RtHohGdJh0L2dqD7mybukXFJxLkdO4';
-}
